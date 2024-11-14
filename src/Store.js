2c3171bf4feef499033350c0a8a8091344cdf2aa
Hamim <hamimsiddiq915@gmail.com>
2024-11-14 09:25:13 +0600

    demo

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import productSlice  from '../Components/Slice/productSlice'
+import { productSlice } from './Components/slice/productSlice'
 
 export const store = configureStore({
   reducer: {
